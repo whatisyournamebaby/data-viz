409cc30a6906bcc3c33a1cd7d7c1330e4f967847
whatisyournamebaby <whatyournamebaby@gmail.com>
2017-02-21 22:40:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  // $('.main *').css('transform','translateY(' + (scrolled*(-0.12)) + 'px)');
+	
+	// $('.main:before').css('height', (scrolled*(-0.15)) + 'px');
+	$('.style').html('<style>.do::before{height:' + scrolled*(.2) + 'px;}</style>');
+	
+	// body.md or lger
+	$('.plx').each( function () {
+  var z = parseInt($(this).data("z"));
+  $(this).css('transform','translateY(' + (scrolled * (z * (-.01))) + 'px)')
+});
+}
+
+    Contact GitHub API Training Shop Blog About 
+
+    © 2017 GitHub, Inc. Terms Priv
