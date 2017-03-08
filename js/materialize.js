be3746ccf1b0d510296a2f4e198db3313c7ad58e
souillade <sophie.ouillade@ensae-paristech.fr>
2017-03-08 17:18:07 +0100

    sommaires

--- js/materialize.js
+++ js/materialize.js
@@ -708,7 +708,18 @@ if ($) {
   }; // End dropdown plugin
 
   $(document).ready(function(){
-    $('.dropdown-button').dropdown();
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    }
+  );
+        
   });
 }( jQuery ));
 ;(function($) {
